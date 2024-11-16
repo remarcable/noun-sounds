206be6f62e575a05c8bfd723864190dcf7661254
Marc N. <git@marcnitzsche.de>
2024-11-16 15:50:25 +0700

    Add bass

--- src/audio/index.ts
+++ src/audio/index.ts
@@ -1,14 +1,40 @@
 import * as Tone from "tone";
-import { loadSynth } from "./instruments";
+// import { loadSynth } from "./instruments";
 
 export const play = async () => {
   await Tone.start();
-  const synth = loadSynth();
-  console.log(synth);
+  // const synth = loadSynth();
 
-  synth.triggerAttackRelease("C4", "16n");
+  Tone.getTransport().start();
+  Tone.getTransport().bpm.value = 130;
+
+  playBassline();
 };
 
-// if (typeof window !== "undefined") {
-//   window.playSynth = play;
-// }
+const playBassline = () => {
+  const bassFilter = new Tone.Filter({ frequency: 600, Q: 8 });
+  const bassEnvelope = new Tone.AmplitudeEnvelope({
+    attack: 0.01,
+    decay: 0.2,
+    sustain: 0,
+  });
+  const gain = new Tone.Gain(0.2);
+  const bass = new Tone.PulseOscillator("A2", 0.4)
+    .chain(bassFilter, bassEnvelope, gain, Tone.getDestination())
+    .start();
+
+  const bassPart = new Tone.Part(
+    (time, note) => {
+      bass.frequency.setValueAtTime(note, time);
+      bassEnvelope.triggerAttack(time);
+    },
+    [
+      ["0:0", "A1"],
+      ["0:2", "G1"],
+      ["0:2:2", "C2"],
+      ["0:3:2", "A1"],
+    ]
+  ).start(0);
+
+  bassPart.loop = true;
+};
