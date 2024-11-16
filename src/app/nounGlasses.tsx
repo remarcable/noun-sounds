3d1eaa647ece8ec01383d4d658eb3a13c18ca97a
Marc N. <git@marcnitzsche.de>
2024-11-16 11:34:38 +0700

    Add headline

---
+++ src/app/nounGlasses.tsx
@@ -0,0 +1,48 @@
+export const NounGlasses = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="320"
+    height="320"
+    shapeRendering="crispEdges"
+    viewBox="0 0 320 320"
+    className={className}
+  >
+    <path
+      fill="#fe500c"
+      d="M100 110h60v10h-60zM170 110h60v10h-60zM100 120h10v10h-10z"
+    ></path>
+    <path fill="#fff" d="M110 120h20v10h-20z"></path>
+    <path d="M130 120h20v10h-20z"></path>
+    <path fill="#fe500c" d="M150 120h10v10h-10zM170 120h10v10h-10z"></path>
+    <path fill="#fff" d="M180 120h20v10h-20z"></path>
+    <path d="M200 120h20v10h-20z"></path>
+    <path fill="#fe500c" d="M220 120h10v10h-10zM70 130h40v10H70z"></path>
+    <path fill="#fff" d="M110 130h20v10h-20z"></path>
+    <path d="M130 130h20v10h-20z"></path>
+    <path fill="#fe500c" d="M150 130h30v10h-30z"></path>
+    <path fill="#fff" d="M180 130h20v10h-20z"></path>
+    <path d="M200 130h20v10h-20z"></path>
+    <path
+      fill="#fe500c"
+      d="M220 130h10v10h-10zM70 140h10v10H70zM100 140h10v10h-10z"
+    ></path>
+    <path fill="#fff" d="M110 140h20v10h-20z"></path>
+    <path d="M130 140h20v10h-20z"></path>
+    <path fill="#fe500c" d="M150 140h10v10h-10zM170 140h10v10h-10z"></path>
+    <path fill="#fff" d="M180 140h20v10h-20z"></path>
+    <path d="M200 140h20v10h-20z"></path>
+    <path
+      fill="#fe500c"
+      d="M220 140h10v10h-10zM70 150h10v10H70zM100 150h10v10h-10z"
+    ></path>
+    <path fill="#fff" d="M110 150h20v10h-20z"></path>
+    <path d="M130 150h20v10h-20z"></path>
+    <path fill="#fe500c" d="M150 150h10v10h-10zM170 150h10v10h-10z"></path>
+    <path fill="#fff" d="M180 150h20v10h-20z"></path>
+    <path d="M200 150h20v10h-20z"></path>
+    <path
+      fill="#fe500c"
+      d="M220 150h10v10h-10zM100 160h60v10h-60zM170 160h60v10h-60z"
+    ></path>
+  </svg>
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { NounGlasses } from "./nounGlasses";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl">Noun Sounds</h1>
+      <div className="flex h-12">
+        <h1 className="text-6xl">Noun Sounds</h1>
+        <NounGlasses className="h-36 w-36 items-center justify-center -mt-7 -ml-2" />
+      </div>
     </div>
   );
 }
